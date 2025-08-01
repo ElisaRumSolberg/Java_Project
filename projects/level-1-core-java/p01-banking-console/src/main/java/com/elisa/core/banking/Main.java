2f0e83026a338685271c6147539cd7160437360c
Elisa Rumeysa Solberg <68196918+ElisaRumSolberg@users.noreply.github.com>
2025-08-01 20:07:00 +0200

    proj01Menu

--- projects/level-1-core-java/p01-banking-console/src/main/java/com/elisa/core/banking/Main.java
+++ projects/level-1-core-java/p01-banking-console/src/main/java/com/elisa/core/banking/Main.java
@@ -1,7 +1,55 @@
 package com.elisa.core.banking;
 
+import java.util.Scanner;
+
 public class Main {
-    static void main() {
+    static double balance ;
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("=== Welcome to the Mini Banking System ===");
+
+            System.out.println("1. View account balance");
+            System.out.println("2. Make a deposit");
+            System.out.println("3. Make a withdraw");
+            System.out.println("4. Exit application");
+            System.out.println();
+            System.out.print("Please enter your choice: ");
+            String input = scan.nextLine().trim();
+
+            try {
+                choice = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid choice. Please enter a number.");
+                choice = -1;
+            }
+
+
+            switch (choice) {
+                case 1-> viewAccountBalance();
+                case 2-> makeDeposit();
+                case 3 -> makeWithdraw();
+                case 4 -> exitMessage();
+                default -> System.out.println("Invalid choice");
+            }
+
+        } while (choice!=4);
+    }
+
+    private static void exitMessage() {
+        System.out.println("Thank you for using our Banking System.");
+    }
+
+    private static void makeWithdraw() {
+    }
+
+    private static void makeDeposit() {
+    }
+
+    private static void viewAccountBalance() {
 
     }
 }
